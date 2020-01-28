2d172b52efc26f7279b14e414af71a9c7b1e3da9
Curtis pachtinger <cpach96@gmail.com>
2020-01-28 15:19:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,28 +1,47 @@
 def create_an_empty_array
-  
+[]
 end
 
 def create_an_array
-  
+  array_dogs = ["bup", "big bupp", "tiny bupp", "big feets"]
 end
 
 def add_element_to_end_of_array(array, element)
+  array_test = []
+  
+  array_test.push "arrays!"
   
 end
 
 def add_element_to_start_of_array(array, element)
   
+  array_test = []
+  
+  array_test.unshift "wow"
+  
 end
 
 def remove_element_from_end_of_array(array)
+  
+  array_test = ["arrays!"]
+  
+  array_test.pop
   
 end
 
 def remove_element_from_start_of_array(array)
   
+  array_test = ["wow"]
+  
+  array_test.shift
+  
 end
 
 def retrieve_element_from_index(array, index_number)
+  
+  array_test = ["am" , "ready"]
+  
+  array_test(0)
   
 end
 
